2d0c1a04b27e4670534cc86964f4a74a8f607d31
Marcio dos Anjos Junior <marciodosanjosjunior@gmail.com>
2023-02-10 18:21:52 -0300

    refactor: reorder styles component

--- src/components/Text/interfaces.ts
+++ src/components/Text/interfaces.ts
@@ -1,11 +1,11 @@
 import { ReactNode } from 'react';
 
 export interface ITextBase {
+  algin?: 'left' | 'center' | 'right';
   children: string | ReactNode;
   color?: `#${string}`;
   fontSize?: number;
   fontWeight?: 100 | 200 | 300 | 400 | 500 | 600 | 700 | 800;
   marginBottom?: number;
   marginTop?: number;
-  algin?: 'left' | 'center' | 'right';
 }

09492c6a9d0b4e744db55acc7b648b7e3a9ce210
Marcio dos Anjos Junior <marciodosanjosjunior@gmail.com>
2023-02-10 10:01:50 -0300

    feature: implements auto ScrollView

--- src/components/Wrapper/index.tsx
+++ src/components/Wrapper/index.tsx
@@ -1,16 +1,36 @@
+import { useState } from 'react';
+import { LayoutChangeEvent, ScrollView, View } from 'react-native';
+import { useNormalize } from '~/hooks/normalize';
 import { IWrapper } from './interfaces';
 import { WrapperBase } from './styles';
 
 export const Wrapper = (props: IWrapper) => {
   const { children, horizontalCentralized, paddingTop, verticalCentralized } = props;
+  const [scrollEnabled, setScrollEnabled] = useState(false);
+
+  const handleOnLayout = (event: LayoutChangeEvent) => {
+    const { height } = event.nativeEvent.layout;
+    const heightNormalized = useNormalize(height, 'height');
+    const heightBase = useNormalize(736, 'height');
+    setScrollEnabled(heightNormalized > heightBase);
+  };
 
   return (
-    <WrapperBase
-      horizontalCentralized={horizontalCentralized}
-      paddingTop={paddingTop}
-      verticalCentralized={verticalCentralized}
+    <ScrollView
+      scrollEnabled={scrollEnabled}
+      showsVerticalScrollIndicator={false}
+      keyboardDismissMode="on-drag"
+      style={{ backgroundColor: '#ffffff' }}
     >
-      {children}
-    </WrapperBase>
+      <View onLayout={handleOnLayout}>
+        <WrapperBase
+          horizontalCentralized={horizontalCentralized}
+          paddingTop={paddingTop}
+          verticalCentralized={verticalCentralized}
+        >
+          {children}
+        </WrapperBase>
+      </View>
+    </ScrollView>
   );
 };

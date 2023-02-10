623b4488fbf81cd7d19940f7a7ead8b45be9fcfa
Marcio dos Anjos Junior <marciodosanjosjunior@gmail.com>
2023-02-10 18:23:03 -0300

    feature: implements SignUpScreen to register new user

--- src/screens/login/sign-up/index.tsx
+++ src/screens/login/sign-up/index.tsx
@@ -4,16 +4,21 @@ import { Link } from '~/components/Link';
 import { Text } from '~/components/Text';
 import { useLinkTo } from '@react-navigation/native';
 import { Wrapper } from '~/components/Wrapper';
+import { CheckBox } from '~/components/CheckBox';
+import { TermGroup } from './styles';
+import { View } from 'react-native';
+import { useState } from 'react';
 
 export const SignUpScreen = () => {
   const linkTo = useLinkTo();
+  const [acceptedTerm, setAcceptedTerm] = useState(false);
 
-  const handleLogin = () => {
-    linkTo('/Main');
+  const handleAcceptTerm = () => {
+    setAcceptedTerm(!acceptedTerm);
   };
 
   return (
-    <Wrapper paddingTop={10}>
+    <Wrapper paddingTop={20} horizontalCentralized>
       <Text algin="center" fontSize={18} fontWeight={600} marginBottom={34} color="#000000">
         Create your account
       </Text>
@@ -21,17 +26,32 @@ export const SignUpScreen = () => {
       <Input label="Last Name" onChange={() => null} placeholder="Last Name" type="text" />
       <Input label="E-mail" onChange={() => null} placeholder="Email" type="text" />
       <Input onChange={() => null} type="password" label="Password" placeholder="Minimum 8 characters" />
-      <Button label="Login" marginTop={25} onPress={handleLogin} />
+      <TermGroup>
+        <View style={{ flexGrow: 0, marginRight: 12 }}>
+          <CheckBox onCheck={handleAcceptTerm} />
+        </View>
+        <View style={{ flexGrow: 1 }}>
+          <Text fontSize={9}>I am over 18 years of age and I have read and agree</Text>
+          <Text>
+            <Text fontSize={9}>to the</Text>{' '}
+            <Text color="#000000" fontSize={9}>
+              Terms of Service
+            </Text>{' '}
+            <Text fontSize={9}>and</Text>{' '}
+            <Text color="#000000" fontSize={9}>
+              Privacy policy.
+            </Text>
+          </Text>
+        </View>
+      </TermGroup>
+      <Button disabled={!acceptedTerm} label="Create account" marginTop={25} onPress={() => linkTo('/Login')} />
       <Text marginTop={13}>
         <Text algin="center" fontSize={12}>
-          Don’t have an account?
+          Already have an account?
         </Text>{' '}
-        <Link algin="center" fontSize={12} onPress={() => linkTo('/Register')}>
-          Sign up
-        </Link>{' '}
-        <Text algin="center" fontSize={12}>
-          here
-        </Text>
+        <Link algin="center" color="#000000" fontSize={12} onPress={() => linkTo('/Login')}>
+          Log in Here
+        </Link>
       </Text>
     </Wrapper>
   );

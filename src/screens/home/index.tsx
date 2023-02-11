83fbb242f5cdebcdead1fb883a47085b881249de
Marcio dos Anjos Junior <marciodosanjosjunior@gmail.com>
2023-02-10 23:09:15 -0300

    fix: change lib victory to victory-native

--- src/screens/home/index.tsx
+++ src/screens/home/index.tsx
@@ -1,10 +1,12 @@
 import * as React from 'react';
 import { Text, View } from 'react-native';
+import { LineChart } from './components/chart';
 
 export const HomeScreen = () => {
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
       <Text>Home!</Text>
+      <LineChart />
     </View>
   );
 };

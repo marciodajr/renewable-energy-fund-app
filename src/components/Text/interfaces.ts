d7127656058943fb40be074710628f6bd02595ec
Marcio dos Anjos Junior <marciodosanjosjunior@gmail.com>
2023-02-11 15:26:41 -0300

    refactor: change interface color to string

--- src/components/Text/interfaces.ts
+++ src/components/Text/interfaces.ts
@@ -3,7 +3,7 @@ import { ReactNode } from 'react';
 export interface ITextBase {
   algin?: 'left' | 'center' | 'right';
   children: string | ReactNode;
-  color?: `#${string}`;
+  color?: string;
   fontSize?: number;
   fontWeight?: 100 | 200 | 300 | 400 | 500 | 600 | 700 | 800;
   marginBottom?: number;

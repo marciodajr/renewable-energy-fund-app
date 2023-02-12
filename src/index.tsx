6370d1000b1516612b947ece6a1cf494436ab17b
Marcio dos Anjos Junior <marciodosanjosjunior@gmail.com>
2023-02-12 10:58:06 -0300

    refactor: readjust navigation and routes

--- src/index.tsx
+++ src/index.tsx
@@ -1,49 +1,66 @@
 import * as React from 'react';
-import { Link, NavigationContainer } from '@react-navigation/native';
+import { NavigationContainer, useLinkTo } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import Ionicons from '@expo/vector-icons/Ionicons';
 import { HomeScreen, PortfolioScreen, SignInScreen, SignUpScreen, TradeScreen } from './screens';
 import { Feather } from '@expo/vector-icons';
 import { normalize } from './utils/normalize';
+import { TouchableOpacity } from 'react-native';
 
 export const Main = () => {
   const Stack = createNativeStackNavigator();
   const Tab = createBottomTabNavigator();
+  const linkTo = useLinkTo();
+
+  const tabBarActiveTintColor = '#770FDF';
+  const tabBarInactiveTintColor = '#000000';
 
   const HomeTabs = () => (
-    <Tab.Navigator>
+    <Tab.Navigator initialRouteName="home">
       <Tab.Screen
-        name="Home"
+        name="home"
         component={HomeScreen}
         options={{
-          //headerShown: false,
+          title: 'Account: $1,457.23',
+          headerShadowVisible: false,
+          headerLeft: () => (
+            <Feather name="user" size={normalize(24)} color="black" style={{ marginLeft: normalize(18) }} />
+          ),
+          headerRight: () => (
+            <Feather name="bell" size={normalize(24)} color="black" style={{ marginRight: normalize(18) }} />
+          ),
           tabBarLabel: 'Home',
           tabBarLabelStyle: {
             fontSize: normalize(10),
             fontFamily: 'Sora-500'
           },
-          tabBarActiveTintColor: '#770FDF',
-          tabBarInactiveTintColor: '#000000',
+          tabBarActiveTintColor,
+          tabBarInactiveTintColor,
           tabBarIcon: ({ color }) => <Feather name="home" size={normalize(20)} color={color} />
         }}
       />
       <Tab.Screen
-        name="Trade"
+        name="trade"
         component={TradeScreen}
-        options={{
+        options={({ navigation }) => ({
+          title: 'Wind Fund',
+          headerLeft: () => (
+            <TouchableOpacity activeOpacity={1} onPress={() => navigation.goBack()}>
+              <Feather name="arrow-left" size={normalize(24)} color="black" style={{ marginLeft: normalize(18) }} />
+            </TouchableOpacity>
+          ),
           tabBarLabel: 'Trade',
           tabBarLabelStyle: {
             fontSize: normalize(10),
             fontFamily: 'Sora-500'
           },
-          tabBarActiveTintColor: '#770FDF',
-          tabBarInactiveTintColor: '#000000',
+          tabBarActiveTintColor,
+          tabBarInactiveTintColor,
           tabBarIcon: ({ color }) => <Feather name="trending-up" size={normalize(20)} color={color} />
-        }}
+        })}
       />
       <Tab.Screen
-        name="Portfolio"
+        name="portfolio"
         component={PortfolioScreen}
         options={{
           tabBarLabel: 'Portfolio',
@@ -51,8 +68,8 @@ export const Main = () => {
             fontSize: normalize(10),
             fontFamily: 'Sora-500'
           },
-          tabBarActiveTintColor: '#770FDF',
-          tabBarInactiveTintColor: '#000000',
+          tabBarActiveTintColor,
+          tabBarInactiveTintColor,
           tabBarIcon: ({ color }) => <Feather name="pie-chart" size={normalize(20)} color={color} />
         }}
       />
@@ -61,21 +78,32 @@ export const Main = () => {
 
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Login">
-        <Stack.Screen name="Main" component={HomeTabs} options={{ headerShown: false }} />
-        <Stack.Screen name="Login" component={SignInScreen} options={{ headerShown: false }} />
+      <Stack.Navigator initialRouteName="login">
         <Stack.Screen
-          name="Register"
-          component={SignUpScreen}
+          name="main"
+          component={HomeTabs}
           options={{
+            headerShown: false,
+            headerShadowVisible: false
+          }}
+        />
+        <Stack.Screen
+          name="login"
+          component={SignInScreen}
+          options={{ headerShown: false, headerShadowVisible: false }}
+        />
+        <Stack.Screen
+          name="register"
+          component={SignUpScreen}
+          options={({ navigation }) => ({
             title: '',
             headerShadowVisible: false,
             headerLeft: () => (
-              <Link to={{ screen: 'Login' }}>
-                <Ionicons name="arrow-back" size={32} />
-              </Link>
+              <TouchableOpacity activeOpacity={1} onPress={() => navigation.goBack()}>
+                <Feather name="arrow-left" size={normalize(24)} color="black" />
+              </TouchableOpacity>
             )
-          }}
+          })}
         />
       </Stack.Navigator>
     </NavigationContainer>

ed4c490ba44d5e861e07c7113bab767148f4e12c
Marcio dos Anjos Junior <marciodosanjosjunior@gmail.com>
2023-02-13 00:20:36 -0300

    feature: add loren ipsum to portfolio

--- src/screens/portfolio/index.tsx
+++ src/screens/portfolio/index.tsx
@@ -1,10 +1,20 @@
 import * as React from 'react';
-import { Text, View } from 'react-native';
+import { Text } from '~/components/Text';
+import { Wrapper } from '~/components/Wrapper';
 
 export const PortfolioScreen = () => {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Portfolio!</Text>
-    </View>
+    <Wrapper paddingTop={120}>
+      <Text fontSize={24} fontWeight={600} marginBottom={12}>
+        Lorem Ipsum
+      </Text>
+      <Text fontSize={14}>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+        magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+        pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est
+        laborum
+      </Text>
+    </Wrapper>
   );
 };

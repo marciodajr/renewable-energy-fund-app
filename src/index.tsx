2ee896615ff1253c5a157a0733c19aa5451886bf
Marcio dos Anjos Junior <marciodosanjosjunior@gmail.com>
2023-02-13 00:20:05 -0300

    feature: adjust routes and headers

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,16 @@
 import * as React from 'react';
-import { NavigationContainer, useLinkTo } from '@react-navigation/native';
+import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { HomeScreen, PortfolioScreen, SignInScreen, SignUpScreen, TradeScreen } from './screens';
 import { Feather } from '@expo/vector-icons';
 import { normalize } from './utils/normalize';
 import { TouchableOpacity } from 'react-native';
+import { Text } from './components/Text';
 
 export const Main = () => {
   const Stack = createNativeStackNavigator();
   const Tab = createBottomTabNavigator();
-  const linkTo = useLinkTo();
 
   const tabBarActiveTintColor = '#770FDF';
   const tabBarInactiveTintColor = '#000000';
@@ -21,7 +21,11 @@ export const Main = () => {
         name="home"
         component={HomeScreen}
         options={{
-          title: 'Account: $1,457.23',
+          headerTitle: () => (
+            <Text color="#000000" fontSize={14} fontWeight={600}>
+              Account: $1,457.23 <Feather name="chevron-down" size={normalize(16)} color="black" />
+            </Text>
+          ),
           headerShadowVisible: false,
           headerLeft: () => (
             <Feather name="user" size={normalize(24)} color="black" style={{ marginLeft: normalize(18) }} />
@@ -44,6 +48,16 @@ export const Main = () => {
         component={TradeScreen}
         options={({ navigation }) => ({
           title: 'Wind Fund',
+          headerTitle: () => (
+            <>
+              <Text color="#000000" fontSize={17} fontWeight={600}>
+                Wind Fund
+              </Text>
+              <Text algin="center" fontSize={14}>
+                WFND
+              </Text>
+            </>
+          ),
           headerLeft: () => (
             <TouchableOpacity activeOpacity={1} onPress={() => navigation.goBack()}>
               <Feather name="arrow-left" size={normalize(24)} color="black" style={{ marginLeft: normalize(18) }} />
@@ -63,6 +77,8 @@ export const Main = () => {
         name="portfolio"
         component={PortfolioScreen}
         options={{
+          headerShown: false,
+          headerShadowVisible: false,
           tabBarLabel: 'Portfolio',
           tabBarLabelStyle: {
             fontSize: normalize(10),
